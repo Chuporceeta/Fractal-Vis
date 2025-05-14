3261ccd881e63817129d3e9c36551630946dcc6c
Xabier <xabier.sienra@gmail.com>
2025-05-14 19:46:25 -0400

    Smoother colors + Zoom with bad centering

--- draw.js
+++ draw.js
@@ -5,7 +5,7 @@ function loadShader(gl, type, source) {
     return shader;
 }
 
-function draw(gl, programInfo, canvas, t) {
+function draw(gl, programInfo, canvas, t, view) {
     canvas.width = window.innerWidth;
     canvas.height = window.innerHeight;
     gl.viewport(0, 0, canvas.width, canvas.height);
@@ -24,13 +24,15 @@ function draw(gl, programInfo, canvas, t) {
 
     gl.useProgram(programInfo.program);
 
+    gl.uniform1f(programInfo.uniformLocations.zoom, view.zoom);
+    gl.uniform2f(programInfo.uniformLocations.center, view.mouseX, view.mouseY);
     gl.uniform2f(programInfo.uniformLocations.dims, canvas.width, canvas.height);
 
     const cx = 0.7885 * Math.cos(t),
           cy = 0.7885 * Math.sin(t);
     gl.uniform2f(programInfo.uniformLocations.c, cx, cy);
     const R = Math.ceil(0.5 + Math.sqrt(1 + 4*Math.sqrt(cx*cx + cy*cy))/2);
-    gl.uniform1f( programInfo.uniformLocations.R, R);
+    gl.uniform1f( programInfo.uniformLocations.R, 2*R);
 
 
     gl.clearColor(0.0, 0.0, 0.0, 1.0);
@@ -60,13 +62,42 @@ function main() {
         },
         uniformLocations: {
             dims: gl.getUniformLocation(shaderProgram, "uDims"),
+            zoom: gl.getUniformLocation(shaderProgram, "uZoom"),
+            center: gl.getUniformLocation(shaderProgram, "uCenter"),
             c: gl.getUniformLocation(shaderProgram, "c"),
             R: gl.getUniformLocation(shaderProgram, "R"),
         }
     }
 
+    let zoom = 1.0, mouseX, mouseY;
+    let view = {
+        zoom: zoom,
+        mouseX: canvas.width / 2,
+        mouseY: canvas.height / 2,
+    }
+    document.addEventListener("mousemove", (e) => {
+        mouseX = e.clientX;
+        mouseY = canvas.height - e.clientY;
+        // console.log(`${mouseX}/${canvas.width}, ${mouseY}/${canvas.height}`);
+    })
+    document.addEventListener("wheel", (e) => {
+        zoom = Math.max(0.01, zoom - 0.001 * e.deltaY);
+        view = {
+            zoom: zoom,
+            mouseX: mouseX,
+            mouseY: mouseY,
+        }
+    })
+
+    let t = 0, paused = false;
+    document.addEventListener("click", () => {paused = !paused;})
+
+    let then = 0;
     function render(now) {
-        draw(gl, programInfo, canvas, now/4000);
+        if (!paused)
+            t += now - then;
+        then = now;
+        draw(gl, programInfo, canvas, t/4000, view);
         requestAnimationFrame(render);
     }
     requestAnimationFrame(render)
