31bd8a9c50c053ce762f43dd1c078c76eb8e89e4
Xabier <xabier.sienra@gmail.com>
2025-05-14 03:54:25 -0400

    Animated visualization of Julia sets

--- draw.js
+++ draw.js
@@ -5,6 +5,40 @@ function loadShader(gl, type, source) {
     return shader;
 }
 
+function draw(gl, programInfo, canvas, now) {
+    canvas.width = window.innerWidth;
+    canvas.height = window.innerHeight;
+    gl.viewport(0, 0, canvas.width, canvas.height);
+
+
+    const posBuffer = gl.createBuffer();
+    gl.bindBuffer(gl.ARRAY_BUFFER, posBuffer);
+    gl.bufferData(
+        gl.ARRAY_BUFFER,
+        new Float32Array([1.0, 1.0, 1.0, -1.0, -1.0, 1.0, -1.0, -1.0]),
+        gl.STATIC_DRAW
+    );
+    gl.vertexAttribPointer(programInfo.attribLocations.vertexPosition, 2, gl.FLOAT, false, 0, 0);
+    gl.enableVertexAttribArray(programInfo.attribLocations.vertexPosition);
+
+
+    gl.useProgram(programInfo.program);
+
+    gl.uniform2f(programInfo.uniformLocations.dims, canvas.width, canvas.height);
+
+    const cx = 0.7885 * Math.cos(now/2000),
+        cy = 0.7885 * Math.sin(now/2000);
+    gl.uniform2f(programInfo.uniformLocations.c, cx, cy);
+    const R = Math.ceil(0.5 + Math.sqrt(1 + 4*Math.sqrt(cx*cx + cy*cy))/2);
+    gl.uniform1f( programInfo.uniformLocations.R, R);
+
+
+    gl.clearColor(0.0, 0.0, 0.0, 1.0);
+    gl.clear(gl.COLOR_BUFFER_BIT);
+
+    gl.drawArrays(gl.TRIANGLE_STRIP, 0, 4);
+}
+
 function main() {
     const canvas = document.getElementById("gl-canvas");
     const vsSource = document.getElementById("vertex-shader").textContent;
@@ -19,24 +53,23 @@ function main() {
     gl.attachShader(shaderProgram, fragmentShader);
     gl.linkProgram(shaderProgram);
 
-    const vertexPosLoc = gl.getAttribLocation(shaderProgram, "aVertexPosition");
+    const programInfo = {
+        program: shaderProgram,
+        attribLocations: {
+            vertexPosition: gl.getAttribLocation(shaderProgram, "aVertexPosition"),
+        },
+        uniformLocations: {
+            dims: gl.getUniformLocation(shaderProgram, "uDims"),
+            c: gl.getUniformLocation(shaderProgram, "c"),
+            R: gl.getUniformLocation(shaderProgram, "R"),
+        }
+    }
 
-    const posBuffer = gl.createBuffer();
-    gl.bindBuffer(gl.ARRAY_BUFFER, posBuffer);
-    gl.bufferData(
-        gl.ARRAY_BUFFER,
-        new Float32Array([1.0, 1.0, 1.0, -1.0, -1.0, 1.0, -1.0, -1.0]),
-        gl.STATIC_DRAW
-    );
-    gl.vertexAttribPointer(vertexPosLoc, 2, gl.FLOAT, false, 0, 0
-    )
-    gl.enableVertexAttribArray(vertexPosLoc);
+    function render(now) {
+        draw(gl, programInfo, canvas, now);
+        requestAnimationFrame(render);
+    }
+    requestAnimationFrame(render);
 
-    gl.clearColor(0.0, 0.0, 0.0, 1.0);
-    gl.clear(gl.COLOR_BUFFER_BIT);
-
-    gl.useProgram(shaderProgram);
-
-    gl.drawArrays(gl.TRIANGLE_STRIP, 0, 4);
 }
 main();
